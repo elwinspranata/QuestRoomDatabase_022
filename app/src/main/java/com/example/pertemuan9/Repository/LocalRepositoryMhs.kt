eef44def26bd343e0009415075b4addd4cee225c
elwinspranata <elwinpranata02@gmail.com>
2024-12-16 21:07:27 +0700

    Menambahkan implementasi LocalRepositoryMhs untuk operasi insertMahasiswa

--- app/src/main/java/com/example/pertemuan9/Repository/LocalRepositoryMhs.kt
+++ app/src/main/java/com/example/pertemuan9/Repository/LocalRepositoryMhs.kt
@@ -1,14 +1,32 @@
 package com.example.pertemuan9.Repository
 
-import com.example.pertemuan9.data.Data.dao.Mahasiswadao
+import com.example.pertemuan9.data.Data.dao.MahasiswaDao
 import com.example.pertemuan9.data.Data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 class LocalRepositoryMhs(
-    private val mahasiswadao: Mahasiswadao
+    private val mahasiswaDao: MahasiswaDao
 ) : RepositoryMhs {
 
-        override suspend fun insertMhs(mahasiswa: Mahasiswa)  {
-            mahasiswadao.insertMahasiswa(mahasiswa)
-        }
+    override suspend fun insertMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.insertMahasiswa(mahasiswa) // Perbaiki jika ada typo di sini
     }
+
+
+    override fun getAllMhs(): Flow<List<Mahasiswa>> {
+        return mahasiswaDao.getAllMahasiswa()
+    }
+
+    override fun getMhs(nim: String): Flow<Mahasiswa> {
+        return mahasiswaDao.getMahasiswa(nim = nim)
+    }
+
+    override suspend fun deleteMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.deleteMahasiswa(mahasiswa)
+    }
+
+    override suspend fun updateMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.updateMahasiswa(mahasiswa)
+    }
+}
 
