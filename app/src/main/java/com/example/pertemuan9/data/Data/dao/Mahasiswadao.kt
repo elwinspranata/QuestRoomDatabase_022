af120c1f6ba954125b32f3a6e337cff508309224
elwinspranata <elwinpranata02@gmail.com>
2024-12-16 20:58:58 +0700

    Menambahkan fungsi insertMahasiswa pada MahasiswaDao

--- app/src/main/java/com/example/pertemuan9/data/Data/dao/Mahasiswadao.kt
+++ app/src/main/java/com/example/pertemuan9/data/Data/dao/Mahasiswadao.kt
@@ -1,12 +1,33 @@
 package com.example.pertemuan9.data.Data.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
 import com.example.pertemuan9.data.Data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 @Dao
-interface Mahasiswadao {
-
+interface MahasiswaDao {
     @Insert
     suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
+
+
+    @Query("SELECT * FROM mahasiswa ORDER BY nama ASC")
+    fun getAllMahasiswa() : Flow<List<Mahasiswa>>
+
+
+    @Query("SELECT * FROM mahasiswa WHERE nim = :nim")
+    fun getMahasiswa(nim: String) : Flow<Mahasiswa>
+
+
+    @Delete
+    suspend fun deleteMahasiswa(mahasiswa: Mahasiswa)
+
+
+    @Update
+    suspend fun updateMahasiswa(mahasiswa: Mahasiswa)
+
+
 }
