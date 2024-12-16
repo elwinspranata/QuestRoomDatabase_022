14cae7a8891557a819abfefecb6632a95191e418
elwinspranata <elwinpranata02@gmail.com>
2024-12-16 21:10:09 +0700

    Menambahkan interface RepositoryMhs untuk operasi CRUD pada data mahasiswa

--- app/src/main/java/com/example/pertemuan9/Repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/pertemuan9/Repository/RepositoryMhs.kt
@@ -1,9 +1,16 @@
 package com.example.pertemuan9.Repository
 
 import com.example.pertemuan9.data.Data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 interface RepositoryMhs {
-
     suspend fun insertMhs(mahasiswa: Mahasiswa)
 
+    fun getAllMhs(): Flow<List<Mahasiswa>>
+
+    fun getMhs(nim: String): Flow<Mahasiswa>
+
+    suspend fun deleteMhs(mahasiswa: Mahasiswa)
+
+    suspend fun updateMhs(mahasiswa: Mahasiswa)
 }
